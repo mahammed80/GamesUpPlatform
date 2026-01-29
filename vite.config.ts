67e9aa2d8d7bddfad58c1d0bc2046c5f827d5622
mahammed80 <mahammed8067@gmail.com>
2026-01-29 13:47:27 +0200

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
